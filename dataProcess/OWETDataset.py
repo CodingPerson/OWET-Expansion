ecdf7b04c8b4a489a2fea1f16354deb767c4f705
Dorian <45897585+CodingPerson@users.noreply.github.com>
2026-08-21 12:32:26 +0800

    Add files via upload

--- dataProcess/OWETDataset.py
+++ dataProcess/OWETDataset.py
@@ -82,7 +82,77 @@ class OWETDataset(Dataset):
 
     def __len__(self):
         return len(self.data)
+class ActiveDataset(Dataset):
+    def __init__(self, data, type2id, tokenizer, num_classes, args: argparse.Namespace, mode='active'):
+        self.mode = mode
+        self.data = data
+        self.type2id = type2id
+        self.tokenizer = tokenizer
 
+        self.len_max_tokenization = 0
+        self.inputs = []
+        self.final_inputs = []
+        self.args = args
+
+        self.targets = np.zeros([len(self.data), num_classes], np.float32)
+        self.top_types = []  # 每个样本的top-level type
+        self.sample_weights = []  # 采样权重
+        self.preprocess()
+
+    def preprocess(self):
+        # {'sentence':[],'mention':{'start':idx,'end':idx,'labels':[],'dtype':xxx}}
+        for i, ins in tqdm.tqdm(enumerate(self.data), desc='preprocess data', total=len(self.data)):
+            context = ins['sentence']
+            if self.args.do_lower:
+                context = [word.lower() for word in context]
+            start = ins['mention']['start']
+            end = ins['mention']['end']
+            mention = context[start:end]
+            leftctx = context[:start]
+            rightctx = context[end:]
+
+            label_id = self.type2id[ins['mention']['labels'][-1]]
+            self.top_types.append(label_id)
+            self.targets[i, label_id] = 1.0
+
+            if len(mention) > self.args.mention_limit:
+                mention = mention[:self.args.mention_limit]
+            mention_ = ' '.join(mention)
+            context_ = ' '.join(leftctx + mention + rightctx)
+            len_tokenization = len(self.tokenizer.encode_plus(mention_, context_)["input_ids"])
+            if len_tokenization > self.args.input_limit:
+                over = len_tokenization - self.args.input_limit
+                rightctx = rightctx[:-over]
+            context_ = ' '.join(leftctx + mention + rightctx)
+            len_tokenization = len(self.tokenizer.encode_plus(mention_, context_)["input_ids"])
+            self.len_max_tokenization = max(self.len_max_tokenization, len_tokenization)
+            self.inputs.append([mention_, context_])
+
+        type2num = Counter(self.top_types)
+        self.sample_weights = [1 / type2num[t] for t in self.top_types]
+        self.sample_weights = torch.DoubleTensor(self.sample_weights)
+
+        self.targets = torch.from_numpy(self.targets)
+        for ins in tqdm.tqdm(self.inputs, desc='encode data for labeled', total=len(self.inputs)):
+            inputs = self.tokenizer.encode_plus(
+                ins[0], ins[1],
+                add_special_tokens=True,
+                # max_length=min(self.args.input_limit, self.len_max_tokenization),
+                # truncation_strategy="only_second",
+                # padding=min(self.args.input_limit, self.len_max_tokenization),
+                return_tensors="pt",
+                # truncation=True
+            )
+            self.final_inputs.append(inputs)
+
+    def __getitem__(self, index):
+        # print(self.inputs[index])
+        # print(index)
+        # print(self.data[index]['mention']['labels'])
+        return self.final_inputs[index], self.targets[index], index
+
+    def __len__(self):
+        return len(self.data)
 
 def labeled_collate_fn(batch):
     inputs, targets, indexes = zip(*batch)
--- dataProcess/loadData.py
+++ dataProcess/loadData.py
@@ -26,7 +26,7 @@ def load(path, level=-1, lower=True):
     return data
 
 
-def getTestValSplit(data, seed, val_ratio=0.2):
+def getTestValSplit(data, seed, val_path=None,label_path=None,val_ratio=0.2,active_ratio=0.2):
     random.seed(seed)
     typeset = set()
     type_idxes = dict()
@@ -43,20 +43,32 @@ def getTestValSplit(data, seed, val_ratio=0.2):
     val_idxes = []
     for k, v in type_idxes.items():
         # print(f'{k} : {len(v) * val_ratio}')
-        if len(v) * val_ratio < 1:
+        if len(v) * active_ratio < 1:
             continue
-        size = round(len(v) * val_ratio)
+        size = round(len(v) * active_ratio)
         # print(f'size: {size}')
         val_idxes.extend(random.sample(v, size))
 
     val_mask = np.zeros(len(data), dtype=bool)
     val_mask[val_idxes] = True
     val_data = data[val_mask]
-    test_data = data[~val_mask]
-    return list(test_data), list(val_data)
+    for ins in val_data:
+        ins['mention']['dtype'] = "extra"
+    lab_data = data[~val_mask]
+
+    with open(val_path, "w", encoding="utf-8") as f_val:
+
+        for val_line in list(val_data):
+            f_val.write(json.dumps(val_line, ensure_ascii=False) + "\n")
+
+    with open(label_path, "w", encoding="utf-8") as f_lab:
+
+        for lab_line in list(lab_data):
+            f_lab.write(json.dumps(lab_line, ensure_ascii=False) + "\n")
+    return list(lab_data), list(val_data)
 
 
-def loadDataset(dataset, seed, split_val=0, val_ratio=0.2):
+def loadDataset(dataset, seed, split_val=0, val_ratio=0.2,active_budget=0.2):
     paths = constant.split_dataPath[dataset]
     if dataset == 'BBN':
         level = 2
@@ -69,17 +81,36 @@ def loadDataset(dataset, seed, split_val=0, val_ratio=0.2):
         lower = True
     else:
         raise Exception(f'not support dataset {dataset}')
-    labeled_data = load(paths[0], level, lower)
-    unlabeled_data = load(paths[1], level, lower)
-    unknown_data = load(paths[2], level, lower)
+    # labeled_data = load(paths[0], level, lower)
+    labeled_known = load(paths[0], level, lower)
+    val_data = load(paths[1], level, lower)
+    unlabeled_known_data = load(paths[2], level, lower)
+    unknown_data = load(paths[3], level, lower)
+    extra_known = load(paths[4], level, lower)
+
+    # extra_path = 'dataset/stdData/'+dataset+'/split/extra_known.json'
+    # label_path = 'dataset/stdData/'+dataset+'/split/labeled.json'
+    # labeled_known, extra_known = getTestValSplit(labeled_data, seed, extra_path, label_path, active_ratio=active_budget)
+
+
+
+    print('labled_known_data')
+    print(len(labeled_known))
+    print('extra_known_data')
+    print(len(extra_known))
+    print('unlabeled_known_data')
+    print(len(unlabeled_known_data))
+    print('val_data')
+    print(len(val_data))
     print('unknown_data')
     print(len(unknown_data))
-    print('unlabeled_data')
-    print(len(unlabeled_data))
-    train_data = unknown_data + unlabeled_data
-    labeled_known, val_data = getTestValSplit(labeled_data, seed, val_ratio=val_ratio)
+    unlabeled_data = unknown_data + unlabeled_known_data
 
-    if split_val == 1:
-        return train_data, labeled_known, val_data
-    else:
-        return train_data, labeled_data, None
+    # val_path = 'dataset/stdData/'+dataset+'/split/val.json'
+    # label_path = 'dataset/stdData/'+dataset+'/split/label.json'
+    # labeled_known, val_data = getTestValSplit(labeled_data, seed, val_path,label_path,val_ratio=val_ratio)
+
+    # if split_val == 1:
+    return unlabeled_data, labeled_known, val_data,extra_known
+    # else:
+    #     return unlabeled_data, labeled_data, None
